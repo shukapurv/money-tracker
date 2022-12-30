e2bd2d951e4e349137b59a4030df78bb0c9d34f7
shukapurv <apoorveshukla2001@gmail.com>
2022-12-30 21:15:27 +0530

    transaction model added

--- backend/core/transaction/models.py
+++ backend/core/transaction/models.py
@@ -7,20 +7,24 @@ from category.models import Category
 
 
 class Transaction(models.Model):
-    users = models.ManyToManyField(User,through=UsersInTransaction,related_name="transactions",related_query_name="transaction")
-    name=models.CharField(max_length=30)
-    category=models.ForeignKey(
-        Category,on_delete=models.SET_NULL,null=True,blank=True
+    users = models.ManyToManyField(User, through="UsersInTransaction",
+                                   related_name="transactions", related_query_name="transaction")
+    name = models.CharField(max_length=30)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True
     )
-    descrition=models.TextField(null=True,blank=True)
-    date=models.DateField(auto_now=True)
+    amount = models.DecimalField(default=0.0, max_digits=7, decimal_places=2)
+    description = models.TextField(null=True, blank=True)
+    date = models.DateField(auto_now=True)
 
-    def __str__(self)->str:
+    def __str__(self) -> str:
         return self.name
 
-class UsersInTransaction():
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
-    transaction=models.ForeignKey(Transaction,on_delete=models.CASCADE)
+
+class UsersInTransaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+    paid = models.BooleanField(default=False)
 
     class Meta:
-        unique_together=("user","transaction")
+        unique_together = ("user", "transaction")
