f83d57e69a7a805e83fe3fc94ed4dd6e778a7691
shukapurv <apoorveshukla2001@gmail.com>
2022-12-30 19:15:33 +0530

    update category app

--- backend/core/authentication/serializers.py
+++ backend/core/authentication/serializers.py
@@ -4,6 +4,27 @@ from rest_framework.validators import UniqueValidator
 from django.contrib.auth import authenticate
 
 
+class DynamicModelSerializer(serializers.ModelSerializer):
+    """
+    A ModelSerializer that takes an additional `fields` argument that
+    controls which fields should be displayed.
+    """
+
+    def __init__(self, *args, **kwargs):
+        # Don't pass the 'fields' arg up to the superclass
+        fields = kwargs.pop("fields", None)
+
+        # Instantiate the superclass normally
+        super().__init__(*args, **kwargs)
+
+        if fields is not None:
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.CharField(
         validators=[UniqueValidator(queryset=User.objects.all())])
@@ -54,8 +75,18 @@ class LoginSerializer(serializers.ModelSerializer):
         return data
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(DynamicModelSerializer):
     class Meta:
         model = User
         exclude = ('password',)
         depth = 2
+
+    @staticmethod
+    def get_minimal_user_fields():
+        return (
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+        )
--- backend/core/category/serializers.py
+++ backend/core/category/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import Category
+from authentication.serializers import UserSerializer
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    user = UserSerializer(
+        read_only=True, fields=UserSerializer.get_minimal_user_fields())
+
     class Meta:
         model = Category
         fields = ["id", "user", "name"]
