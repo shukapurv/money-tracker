f95fbcf240d9b91b4300bad42f8de7bad462810e
shukapurv <apoorveshukla2001@gmail.com>
2022-12-30 16:28:06 +0530

    swagger update

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -146,3 +146,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Token': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    }
+}
