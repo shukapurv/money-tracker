6405a81178f7daa8989791d88f785179e32c091f
shukapurv <apoorveshukla2001@gmail.com>
2022-12-30 17:07:07 +0530

    Category app added

---
+++ backend/core/category/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+# Create your models here.
+
+
+class Category(models.Model):
+    user = models.ForeignKey(
+        User, related_name="categories", on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True)
+
+    def __str__(self) -> str:
+        ind = self.name + ':' + self.user
+        return ind
+
+
+@receiver(post_save, sender=User)
+def create_default_categories(sender, instance, created, **kwargs) -> None:
+    if created:
+        Category.objects.create(user=instance, name="Electricity")
+        Category.objects.create(user=instance, name="Laundry")
+        Category.objects.create(user=instance, name="Transport")
+        Category.objects.create(user=instance, name="Food")
+        Category.objects.create(user=instance, name="Groccery")
